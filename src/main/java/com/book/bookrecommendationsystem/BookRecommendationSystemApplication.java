ba1c9bef41a9f760527fa2e34a2404a32cab8087
sergiopoliveira <sergiopoliveira@outlook.com>
2018-08-06 15:06:20 +0100

    Book Recommendation System

---
+++ src/main/java/com/book/bookrecommendationsystem/BookRecommendationSystemApplication.java
@@ -0,0 +1,12 @@
+package com.book.bookrecommendationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookRecommendationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookRecommendationSystemApplication.class, args);
+	}
+}
